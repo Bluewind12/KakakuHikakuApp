54268be5149726ff2c5842311ddef4014fae1e33
BlueWind <zero18030@gmail.com>
2018-10-11 00:15:15 +0900

    設定画面の動作の作成

--- app/src/main/java/com/example/momoproject/kakakuhikaku/SettingActivity.kt
+++ app/src/main/java/com/example/momoproject/kakakuhikaku/SettingActivity.kt
@@ -3,25 +3,29 @@ package com.example.momoproject.kakakuhikaku
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.android.synthetic.main.setting_layout.*
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.setting_layout)
 
         val settingData = getSharedPreferences("SettingData", Context.MODE_PRIVATE)
         val edit = settingData.edit()
 
-        settingData.getString("UP_URL", "https://www.amazon.co.jp/gp/aw/s/ref=nb_sb_noss?k=")
-        settingData.getString("DOWN_URL", "https://search.rakuten.co.jp/search/mall/")
+        upEditText.setText(settingData.getString("UP_URL", "https://www.amazon.co.jp/gp/aw/s/ref=nb_sb_noss?k="), TextView.BufferType.NORMAL)
+        downEditText.setText(settingData.getString("DOWN_URL", "https://search.rakuten.co.jp/search/mall/"), TextView.BufferType.NORMAL)
 
         settingEnterButton.setOnClickListener {
-
+            edit.putString("UP_URL", upEditText.text.toString())
+            edit.putString("DOWN_URL", downEditText.text.toString())
+            edit.apply()
         }
 
         resetButton.setOnClickListener {
-
+            upEditText.setText("https://www.amazon.co.jp/gp/aw/s/ref=nb_sb_noss?k=", TextView.BufferType.NORMAL)
+            downEditText.setText("https://search.rakuten.co.jp/search/mall/", TextView.BufferType.NORMAL)
         }
     }
 }
